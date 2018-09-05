38c732aab222beadb4fb16ab92a8c18f84dd05fa
Vishal D <vishaldiwadkar@protonmail.com>
2018-09-05 03:13:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,8 +2,8 @@ function forLoop(arg) {
   for (var i = 0; i < 25; i++) {
   if (i === 1) {
    arg[i] = "I am 1 strange loop.";
-  }else {
-   arg[i] = "I am ${i} strange loop."; 
+  } else {
+    arg[i] = "I am ${i} strange loop."; 
   }
 }
 return arg
